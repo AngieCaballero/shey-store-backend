452e27936f77a9d0d284223dfd0bd8569134a10b
AngieCaballero <karma2000angiejaz@gmail.com>
2024-06-08 18:56:26 -0600

    Config Swagger Module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
+import { ResponseInterceptor } from './common/response.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Shey Store API')
+    .setDescription('Shey Store API Doc')
+    .setVersion('1.0')
+    .addTag('Shey Store')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, document)
+
+  app.useGlobalInterceptors(new ResponseInterceptor())
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
